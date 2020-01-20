f9f85b2ad8a0f34b16f2928c0a9c759d0d45f52f
Oskar Rosen <oskar@oskarrosen.com>
2020-01-20 21:55:02 +0100

    added type module to package json and added babel and setup express server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 8002
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
